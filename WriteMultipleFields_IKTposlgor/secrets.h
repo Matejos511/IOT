3c921b2e31a3d3ae6a69836058bb811d3075dda8
Matejos511 <matej.sorsek.m@gmail.com>
2024-10-25 12:24:03 +0200

    Add files via upload

---
+++ WriteMultipleFields_IKTposlgor/secrets.h
@@ -0,0 +1,9 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+#define SECRET_MAC {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x19}
+
+#define SECRET_CH_ID  2714604			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "BJ3E66PE6924PPIG"   // replace XYZ with your channel write API Key
